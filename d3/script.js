caf5856c41cb69c7bf926100a6f075d361da5f1c
Eric Weger <ericweger@gmail.com>
2017-06-15 22:15:02 -0700

    Add test template. Start email build scripts.

--- d3/script.js
+++ d3/script.js
@@ -42,3 +42,121 @@ function createOutline(colors) {
 }
 
 createOutline(colors);
+
+
+var email = {
+	id: "test0001",
+	styles: {},
+	breakpoints: {},
+	idCounter: 3,
+	components: [{
+		type: "fullWidth",
+		id: "0001",
+		styles: [
+			{name: "background-color", value: "#039BE5"}
+		],
+		tableAttributes: [
+			{name: "height", value: "100%"},
+			{name: "width", value: "100%"},
+			{name: "cellspacing", value: "0"},
+			{name: "cellpadding", value: "0"},
+			{name: "border", value: "0"}
+		],
+		cellAttributes: [
+			{name: "align", value: "center"},
+			{name: "valign", value: "top"}
+		],
+		children: [{
+			type: "setWidth",
+			id: "0002",
+			styles: [
+				{name: "background-color", value: "#f0f0f0"},
+				{name: "max-width", value: "100%"}
+			],
+			tableAttributes: [
+				{name: "width", value: "600px"},
+				{name: "cellspacing", value: "0"},
+				{name: "cellpadding", value: "0"},
+				{name: "border", value: "0"}
+			],
+			cellAttributes: [
+				{name: "height", value: "200px"},
+				{name: "align", value: "center"},
+				{name: "valign", value: "top"}
+			],
+			children: [
+				"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim aliquam, ea voluptatem delectus, fugiat quidem possimus ab dignissimos vitae placeat dolores rerum minima laudantium pariatur magni consectetur aut facere sapiente."
+			]
+		},
+			{
+			type: "setWidth",
+			id: "0003",
+			styles: [
+				{name: "background-color", value: "#f0f0f0"},
+				{name: "max-width", value: "100%"}
+			],
+			tableAttributes: [
+				{name: "width", value: "600px"},
+				{name: "cellspacing", value: "0"},
+				{name: "cellpadding", value: "0"},
+				{name: "border", value: "0"}
+			],
+			cellAttributes: [
+				{name: "height", value: "200px"},
+				{name: "align", value: "center"},
+				{name: "valign", value: "top"}
+			],
+			children: [
+				"Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+			]
+			}]
+	}]
+}
+
+function buildEmail(content) {
+	var preview = $(".preview.page-section:first");
+
+}
+
+function buildSection(content) {
+	if (content.type) {
+		switch (content.type) {
+			case "fullWidth":
+				var elem = buildFullWidth(content);
+				break;
+			case "setWidth":
+				var elem = buildSetWidth(content);
+				break;
+			default:
+				var elem = undefined;
+							}
+	} else {
+		return buildText(content);
+	}
+}
+
+function childrenCheck(content) {
+	if (content.children) {
+		if (Array.isArray(content.children) && content.children.length > 0) {
+			for (var i = 0; i < content.children.length; i++) {
+				var elem = buildSection(content.children[i]);
+				// Move this whole section into each build function if actions will change on each.
+			}
+		}
+	}
+}
+
+function buildFullWidth(content) {
+
+}
+
+function buildSetWidth(content) {
+
+}
+
+function buildText(content) {
+
+}
+
+
+buildEmail(email);
